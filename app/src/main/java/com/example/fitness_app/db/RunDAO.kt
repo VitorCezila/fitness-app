e36123ccc2f22cd6cc135505b77e94d9a7fa635e
Vitor Cezila <vcezila@gmail.com>
2021-03-05 21:43:14 -0300

    Adding more functions to the DAO

--- app/src/main/java/com/example/fitness_app/db/RunDAO.kt
+++ app/src/main/java/com/example/fitness_app/db/RunDAO.kt
@@ -30,6 +30,16 @@ interface RunDAO {
     fun getAllRunsSortedByDistance(): LiveData<List<Run>>
 
 
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
     fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
     
 }
